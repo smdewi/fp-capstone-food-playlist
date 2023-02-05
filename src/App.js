7ef2fdd76cbca9d2b2a585fa351053a722cc1135
smdewi <smdewi2020@gmail.com>
2023-02-05 14:20:56 +0800

    set up basic routes for Login, SignUp and Playlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { SignUp } from './pages/SignUp'
+import { Playlist } from './pages/Playlist';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Login Page</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/playlist" element={<Playlist />} />
+    </Routes>
   );
 }
 

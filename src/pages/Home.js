8a4bca0b9201356767b23bbc258ad970f7dd6cb4
phayhy <phay.hy@gmail.com>
2023-02-08 11:32:07 +0800

    Create main landing/home page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import VendorCardTiles from "../components/VendorCardTiles";
+
+export function Home() {
+    return (
+        <div className="App">
+            <VendorCardTiles></VendorCardTiles>
+        </div>
+    );
+}

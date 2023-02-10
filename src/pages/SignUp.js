a42183e590b2c8a9cffc3d19a073b9e96b83f77e
phayhy <phay.hy@gmail.com>
2023-02-11 03:51:37 +0800

    Create first sign up page with email

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,46 @@
+import {
+  Container,
+  Form,  
+  FloatingLabel,
+  Image,
+} from "react-bootstrap";
+
+import ButtonNavigate from "../components/ButtonNavigate";
+import backArrow from "../assets/icons/arrows/ic-arrow-tail-back.svg";
+import email from "../assets/image/login/img-email.svg";
+
 export function SignUp() {
-    return (
-        <div className="App">
-            <h1>Sign up Page</h1>
-        </div>
-    );
+  return (
+    <Container className="container-shadow" style={{ width: "380px" }}>
+      <Form>
+        <ButtonNavigate
+          class="btn-back"
+          path="/login"
+          imgSrc={backArrow}
+          imgAlt="Back Arrow"
+        />
+        <br />
+        <Image className="img-small" src={email} alt="Icons" />
+        <h2 className="f-24 fw-bold mb-xs">What's your email?</h2>
+        <h3 className="f-14 fw-light mb-lg">
+          We'll check if you have an account
+        </h3>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control type="email" placeholder="name@example.com" />
+          </FloatingLabel>
+        </Form.Group>
+        <ButtonNavigate
+          class="btn btn-main my-2"
+          type="submit"
+          path="/account"
+          text="Continue with email"
+        />
+      </Form>
+    </Container>
+  );
 }

5998ca49280993078a1d1aef342879d8c8ed0b8e
phayhy <phay.hy@gmail.com>
2023-02-12 04:14:19 +0800

    Update buttons to include more images

--- src/components/ButtonLogin.js
+++ src/components/ButtonLogin.js
@@ -20,16 +20,34 @@ function ButtonLogin(props) {
 
   return (
     <Button
-      className= {props.className}
-      variant={props.variant}         
+      className={props.class}
+      variant={props.variant}
       disabled={isLoading}
       onClick={!isLoading ? handleClick : null}
     >
-      {isLoading ? "Loading…" : <Image src={props.imgSrc} alt={props.imgAlt}/>}
-      {isLoading ? "" : `\u00a0 ${props.text}`}
+      {isLoading ? (
+        <div class="mx-auto">Loading…</div>
+      ) : (
+        <div class="row">
+          <div class="col-2 p-1">
+            <Image
+              className={props.imgClass}
+              src={props.imgSrc}
+              alt={props.imgAlt}
+            />
+          </div>
+          <div class="col-8 p-1">{isLoading ? "" : `${props.text}`}</div>
+          <div class="col-2 p-1">
+          <Image
+              className={props.imgClassSec}
+              src={props.imgSrcSec}
+              alt={props.imgAltSec}
+            />
+          </div>
+        </div>
+      )}
     </Button>
   );
 }
 
 export default ButtonLogin;
-
--- src/components/ButtonNavigate.js
+++ src/components/ButtonNavigate.js
@@ -10,8 +10,23 @@ function ButtonNavigate(props) {
 
   return (
     <Button className={props.class} type={props.submit} onClick={handleClick}>
-      <Image src={props.imgSrc} alt={props.imgAlt}/>
-      {props.text}
+      <div class="row">
+          <div class="col-2 p-1">
+          <Image
+            className={props.imgClass}
+            src={props.imgSrc}
+            alt={props.imgAlt}
+          />
+        </div>
+        <div class="col-8 p-1">{props.text}</div>
+        <div class="col-2 p-1">
+          <Image
+            className={props.imgClassSec}
+            src={props.imgSrcSec}
+            alt={props.imgAltSec}
+          />
+        </div>
+      </div>
     </Button>
   );
 }

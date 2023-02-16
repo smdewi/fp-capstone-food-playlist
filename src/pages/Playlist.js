3163100c8849722732ee0d6ba8b6fda6bee45841
smdewi <smdewi2020@gmail.com>
2023-02-16 22:47:25 +0800

    add menu item

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -4,8 +4,8 @@ import { Card, Container, Row, Col, Badge } from "react-bootstrap";
 
 export function Playlist() {
     return (
-        <Container fluid>
-            <h1>Playlist Page</h1>
+        <Container fluid style={{ marginTop: "3em"}}>
+
             <Container className="container-shadow" style={{ width: "75%" }}>
                 <Row>
                     <Col xs={4}>Placeholder Image</Col>
@@ -20,8 +20,21 @@ export function Playlist() {
                         <Badge pill bg="info" style={{ marginRight: "1em"}}>Western</Badge>
                         <Badge pill bg="info" style={{ marginRight: "1em"}}>Chinese</Badge>
                     </Col>
+                </Row> 
+            </Container>
+
+            <Container className="container-shadow" style={{ width: "60%", marginTop: "2em" }}>
+                <Row>
+                    <Col xs={8}>
+                        <Card.Title>Mushroom Soup</Card.Title>
+                        <br />
+                        <Card.Body>Menu item description</Card.Body>
+                        <Card.Body>$12.80</Card.Body>
+                        <br />
+                        <Card.Body>From: Soup Spoon</Card.Body>
+                    </Col>
+                    <Col xs={4}>Placeholder Image</Col>
                 </Row>
-                
             </Container>
         </Container>
         

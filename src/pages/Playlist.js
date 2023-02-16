949f69c4b8d4a314656f55224e1ea52b214cb67e
smdewi <smdewi2020@gmail.com>
2023-02-16 22:17:37 +0800

    Playlist Card display

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { Card, Container, Row, Col, Badge } from "react-bootstrap";
+
+
 export function Playlist() {
     return (
-        <div className="App">
+        <Container fluid>
             <h1>Playlist Page</h1>
-        </div>
+            <Container className="container-shadow" style={{ width: "75%" }}>
+                <Row>
+                    <Col xs={4}>Placeholder Image</Col>
+                    <Col xs={8}>
+                        <Card.Title>Rainy Day Playlist</Card.Title>
+                        <Card.Body>2 / 5</Card.Body>
+                        <br />
+                        <Card.Body>starts on 1 Feb 2023</Card.Body>
+                        <Card.Body>ends on 1 Mar 2023</Card.Body>
+                        <br />
+                        <Badge pill bg="info" style={{ marginRight: "1em"}}>Soup</Badge>
+                        <Badge pill bg="info" style={{ marginRight: "1em"}}>Western</Badge>
+                        <Badge pill bg="info" style={{ marginRight: "1em"}}>Chinese</Badge>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </Container>
+        
     );
 }

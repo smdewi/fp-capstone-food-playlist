b201cd4ce15475ac69d5e815a0cd7ebf0e21de0a
phayhy <phay.hy@gmail.com>
2023-02-21 20:38:08 +0800

    Update VendorCardTiles, CSS styles, Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import VendorCardTiles from "../components/VendorCardTiles";
+import SwimLane from "../components/SwimLane";
 
 export function Home() {
     return (
-        <div className="App">
+        <div className="App">            
+            <SwimLane></SwimLane>
+            <br/>
             <VendorCardTiles></VendorCardTiles>
         </div>
     );

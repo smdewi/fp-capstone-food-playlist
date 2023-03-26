b232dad0bc6c3ab9caf1a0fd01e2e39478179532
smdewi <smdewi2020@gmail.com>
2023-03-26 21:56:55 +0800

    create new user

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
 import {
   Container,
   Form,
@@ -12,9 +14,25 @@ import backArrow from "../assets/icons/arrows/ic-arrow-tail-back.svg";
 import createAccount from "../assets/image/login/img-create-new-account.svg";
 
 export function Account() {
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    Axios.post('https://pau-pau-food-playlist-api.onrender.com/user', {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    });
+  }
+
   return (
     <Container className="container-shadow" style={{ width: "380px" }}>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <ButtonNavigate
           class="btn-back"
           path="/signup"
@@ -25,9 +43,9 @@ export function Account() {
         <br />
         <Image className="img-small" src={createAccount} alt="Icons" />
         <h2 className="f-24 fw-bold mb-xs">Let's get you started!</h2>
-        <h3 className="f-14 fw-light mb-lg">
+        {/* <h3 className="f-14 fw-light mb-lg">
           First, let's create your foodpanda account with paupau@hungry.com
-        </h3>
+        </h3> */}
         <Row>
           <Col>
             <Form.Group className="mb-3" controlId="formFirstName">
@@ -36,7 +54,7 @@ export function Account() {
                 label="First name"
                 className="mb-3"
               >
-                <Form.Control placeholder="First name" />
+                <Form.Control placeholder="First name" required onChange={(e) => {setFirstName(e.target.value)}}/>
               </FloatingLabel>
             </Form.Group>
           </Col>
@@ -47,18 +65,29 @@ export function Account() {
                 label="Last name"
                 className="mb-3"
               >
-                <Form.Control placeholder="Last name" />
+                <Form.Control placeholder="Last name" required onChange={(e) => {setLastName(e.target.value)}}/>
               </FloatingLabel>
             </Form.Group>
           </Col>
         </Row>
+
+        <Form.Group className="mb-3" controlId="formEmail">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control type="email" placeholder="name@example.com" required onChange={(e) => {setEmail(e.target.value)}}/>
+          </FloatingLabel>
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formPassword">
           <FloatingLabel
             controlId="floatingInput"
             label="Password"
             className="mb-3"
           >
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" required onChange={(e) => {setPassword(e.target.value)}} />
           </FloatingLabel>
         </Form.Group>
         <ButtonNavigate
